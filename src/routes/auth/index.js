78d1f4062aea0c862debce407ff1767401b2f806
tallninja <tallninja>
2021-11-10 13:52:06 +0300

    made some few refactors on the reset password route

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -5,11 +5,13 @@ const signinRoute = require("./signin.route");
 const signoutRoute = require("./signout.route");
 const profileRoute = require("./profile.route");
 const forgotPasswordRoute = require("./forgotPassword.route");
+const resetPasswordRoute = require("./resetPassword.route");
 
 router.use("/signup", signupRoute);
 router.use("/signin", signinRoute);
 router.use("/signout", signoutRoute);
 router.use("/profile", profileRoute);
 router.use("/forgotpassword", forgotPasswordRoute);
+router.use("/resetpassword", resetPasswordRoute);
 
 module.exports = router;
