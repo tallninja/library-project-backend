e3e84569821e6603e698123009933cf8ec81a4d2
tallninja <tallninja>
2021-11-16 20:10:51 +0300

    fixed the issue on dependency with the dev keys

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,4 +1,5 @@
-const devKeys = require("./dev");
-const prodKeys = require("./prod");
-
-module.exports = process.env.NODE_ENV === "production" ? prodKeys : devKeys;
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./prod.js");
+} else {
+  module.exports = require("./dev.js");
+}
