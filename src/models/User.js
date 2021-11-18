9bfb6499aebd8b7e642a895853b94885c2a9cd1d
tallninja <tallninja>
2021-11-18 05:50:36 +0300

    changed the user model

--- src/models/User.js
+++ src/models/User.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
   email: {
     type: String,
     required: true,
@@ -10,11 +16,15 @@ const UserSchema = new mongoose.Schema({
   },
   phoneNumber: String,
   address: String,
-  password: String,
+  password: {
+    type: String,
+    required: true,
+  },
   roles: [
     {
       type: mongoose.Schema.Types.ObjectId,
       ref: "roles",
+      required: true,
     },
   ],
 });
