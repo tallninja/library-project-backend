9aba9a48d0401b6fa49506da0262871215993266
tallninja <tallninja123@gmail.com>
2021-11-23 16:15:48 +0300

    still trying to resolve session issues

--- src/App.js
+++ src/App.js
@@ -17,19 +17,7 @@ class App {
   }
 
   setup = () => {
-    const whitelist = ["http://localhost:3000"];
-    const corsOptions = {
-      origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1) {
-          callback(null, true);
-        } else {
-          callback(new Error("Not allowed by CORS"));
-        }
-      },
-      credentials: true,
-    };
-
-    this.app.use(cors(corsOptions)); // handle CORS requests
+    // this.app.use(cors()); // handle CORS requests
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json()); // replaced bodyparser
     this.app.use(
